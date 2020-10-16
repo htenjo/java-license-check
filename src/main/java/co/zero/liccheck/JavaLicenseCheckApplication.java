c117d7a1823e5c9644f115dd260b8296682940d5
Hernan Tenjo <hernan.tenjo@globant.com>
2020-10-16 08:43:54 -0500

    before meli

---
+++ src/main/java/co/zero/liccheck/JavaLicenseCheckApplication.java
@@ -0,0 +1,13 @@
+package co.zero.liccheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLicenseCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLicenseCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/zero/liccheck/JavaLicenseCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package co.zero.liccheck;
+
+import org.junit.jupiter.api.Test;
+
+//@SpringBootTest
+class JavaLicenseCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
